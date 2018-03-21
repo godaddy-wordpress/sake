61089631084de7880a2e78a43812853360081067
ragulka <illimar.tambek@gmail.com>
2018-03-21 22:42:55 +0200

    Add püropery option to config task

--- tasks/config.js
+++ tasks/config.js
@@ -1,9 +1,15 @@
-const log = require('fancy-log')
+const dottie = require('dottie')
 
-module.exports = (gulp, config) => {
+module.exports = (gulp, config, plugins, options) => {
   // prints current configuration
   gulp.task('config', (done) => {
-    console.log(config)
+    // pass --property=deploy.production to only see config values for that propery
+    if (options.property) {
+      console.log(dottie.get(config, options.property))
+    } else {
+      console.log(config)
+    }
+
     done()
   })
 }
