745796d04fc9096c24ce008f6c9662747646bb99
ragulka <illimar.tambek@gmail.com>
2018-03-28 22:03:50 +0300

    Add option to skip composer install during build

--- tasks/build.js
+++ tasks/build.js
@@ -1,7 +1,15 @@
-module.exports = (gulp) => {
+const _ = require('lodash')
+
+module.exports = (gulp, plugins, sake) => {
   // main task for building the plugin:
   // - cleans the build directory
   // - compiles the plugin assets (linting where necessary)
   // - copies plugin files to the build directory
-  return gulp.task('build', gulp.series('clean:build', 'shell:composer_install', 'compile', 'copy:build'))
+  let tasks = ['clean:build', 'shell:composer_install', 'compile', 'copy:build']
+
+  if (sake.options['skip-composer']) {
+    tasks = _.without(tasks, 'shell:composer_install')
+  }
+
+  return gulp.task('build', gulp.series(tasks))
 }
