cc19ac6916f792d06f9507a11fc4aaafed664dbc
ragulka <illimar.tambek@gmail.com>
2018-05-30 13:14:14 +0300

    Try resolving gulp bin location

--- bin/sake.js
+++ bin/sake.js
@@ -6,7 +6,6 @@ const path = require('path')
 const gulpfile = path.join(__dirname, '../gulpfile.js')
 // pass args through to gulp
 const args = process.argv.splice(2).concat(['--gulpfile', gulpfile, '--cwd', process.cwd()])
-// use local copy of gulp
-const gulpPath = path.join(__dirname, '../node_modules/.bin/gulp')
+
 // fire up gulp
-spawn(gulpPath, args, { cwd: process.cwd(), stdio: 'inherit' })
+spawn(require('resolve-bin').sync('gulp'), args, { cwd: process.cwd(), stdio: 'inherit' })
