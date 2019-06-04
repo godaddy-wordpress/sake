fba77370bd5da09ef1d0b640ea4b0bb5f906475a
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2019-06-04 15:45:11 +0800

    Save to .js

--- tasks/decaffeinate.js
+++ tasks/decaffeinate.js
@@ -4,5 +4,7 @@ module.exports = (gulp, plugins, sake) => {
   gulp.task('decaffeinate', () => {
     return gulp.src(`${sake.config.paths.assetPaths.js}/**/*.coffee`)
       .pipe(plugins.coffee({ bare: false }))
+      .pipe(gulp.dest(sake.config.paths.assetPaths.js))
+      .pipe(plugins.if(() => sake.isWatching && sake.config.tasks.watch.useBrowserSync, plugins.browserSync.stream.apply({ match: '**/*.js' })))
   })
 }
