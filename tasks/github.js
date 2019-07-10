e07a8fa0199854a732acad22fe266ef28e2838aa
Illimar Tambek <illimar.tambek@gmail.com>
2019-07-10 10:40:26 +0300

    Update to latest ocotkit/rest and address deprecations

--- tasks/github.js
+++ tasks/github.js
@@ -9,16 +9,18 @@ const dateFormat = require('dateformat')
 const log = require('fancy-log')
 
 module.exports = (gulp, plugins, sake) => {
+
+  const github = new GitHub({
+    debug: false,
+    auth: {
+      username: process.env.GITHUB_USERNAME,
+      password: process.env.GITHUB_API_KEY
+    }
+  })
+
   gulp.task('github:get_rissue', (done) => {
     let owner = sake.config.deploy.dev.owner
     let repo = sake.config.deploy.dev.name
-    let github = new GitHub({ debug: false })
-
-    github.authenticate({
-      type: 'basic',
-      username: process.env.GITHUB_USERNAME,
-      password: process.env.GITHUB_API_KEY
-    })
 
     let labels = ['release']
 
@@ -26,75 +28,63 @@ module.exports = (gulp, plugins, sake) => {
       labels.push(sake.config.plugin.id.replace('woocommerce-', ''))
     }
 
-    github.issues.getForRepo({
+    github.issues.listForRepo({
       owner: owner,
       repo: repo,
       state: 'open',
       labels: labels.join(',')
-    }, function (err, result) {
-      if (err) {
-        log.error('Could not get release issue: ' + err.toString())
+    }).then((result) => {
+      if (!result.data.length) {
         done()
       } else {
-        if (!result.data.length) {
+        inquirer.prompt([{
+          type: 'list',
+          name: 'issues_to_close',
+          message: 'Release issues exist for ' + sake.getPluginName() + '. Select an issue this release should close.',
+          choices: function () {
+            let choices = result.data.map((result) => {
+              return {
+                value: result.number,
+                name: 'Close issue #' + result.number + ': ' + result.html_url
+              }
+            })
+
+            choices.push({
+              value: 'none',
+              name: 'None'.red
+            })
+
+            return choices
+          }
+        }]).then(function (answers) {
+          if (answers.issues_to_close === 'none') {
+            log.warn('No issues will be closed for release of ' + sake.getPluginName())
+          } else {
+            sake.options.release_issue_to_close = answers.issues_to_close
+          }
           done()
-        } else {
-          inquirer.prompt([ {
-            type: 'list',
-            name: 'issues_to_close',
-            message: 'Release issues exist for ' + sake.getPluginName() + '. Select an issue this release should close.',
-            choices: function () {
-              let choices = result.data.map((result) => {
-                return {
-                  value: result.number,
-                  name: 'Close issue #' + result.number + ': ' + result.html_url
-                }
-              })
-
-              choices.push({
-                value: 'none',
-                name: 'None'.red
-              })
-
-              return choices
-            }
-          } ]).then(function (answers) {
-            if (answers.issues_to_close === 'none') {
-              log.warn('No issues will be closed for release of ' + sake.getPluginName())
-            } else {
-              sake.options.release_issue_to_close = answers.issues_to_close
-            }
-            done()
-          })
-        }
+        })
       }
+    }).catch((err) => {
+      log.error('Could not get release issue: ' + err.toString())
+      done()
     })
   })
 
   gulp.task('github:get_wc_issues', (done) => {
     let owner = sake.config.deploy.production.owner
     let repo = sake.config.deploy.production.name
-    let github = new GitHub({ debug: false })
 
-    github.authenticate({
-      type: 'basic',
-      username: process.env.GITHUB_USERNAME,
-      password: process.env.GITHUB_API_KEY
-    })
-
-    github.issues.getForRepo({
+    github.issues.listForRepo({
       owner: owner,
       repo: repo,
       state: 'open'
-    }, function (err, result) {
-      if (err) {
-        log.error('Could not get issues for WC repo: ' + err.toString())
-        done()
-      } else {
+    })
+      .then((result) => {
         if (!result.data.length) {
           done()
         } else {
-          inquirer.prompt([ {
+          inquirer.prompt([{
             type: 'checkbox',
             name: 'issues_to_close',
             message: 'Issues on WC repo exist for ' + sake.getPluginName() + '. Select an issue this release should close.',
@@ -108,7 +98,7 @@ module.exports = (gulp, plugins, sake) => {
 
               return choices
             }
-          } ]).then(function (answers) {
+          }]).then(function (answers) {
             if (answers.issues_to_close === 'none') {
               log.warn('No issues will be closed for release of ' + sake.getPluginName())
             } else {
@@ -117,21 +107,17 @@ module.exports = (gulp, plugins, sake) => {
             done()
           })
         }
-      }
-    })
+      })
+      .catch((err) => {
+        log.error('Could not get issues for WC repo: ' + err.toString())
+        done()
+      })
   })
 
   // creates a docs issue for the plugin
   gulp.task('github:docs_issue', (done) => {
     let owner = 'skyverge'
     let repo = 'wc-plugins-sales-docs'
-    let github = new GitHub({ debug: false })
-
-    github.authenticate({
-      type: 'basic',
-      username: process.env.GITHUB_USERNAME,
-      password: process.env.GITHUB_API_KEY
-    })
 
     let message = 'Should a Docs issue be created for ' + sake.getPluginName() + '?'
 
@@ -139,7 +125,7 @@ module.exports = (gulp, plugins, sake) => {
       message += chalk.yellow('\n\nThe changelog below contains new features and/or tweaks.\nA docs issue should always be created for releases with new features or user-facing changes.')
     }
 
-    inquirer.prompt([ {
+    inquirer.prompt([{
       type: 'list',
       name: 'create_docs_issue',
       message: message + '\n\nChangelog: \n\n' + sake.getPluginChanges() + '\n\n',
@@ -151,22 +137,20 @@ module.exports = (gulp, plugins, sake) => {
         name: "No -- don't create a docs issue!"
       }],
       default: sake.pluginHasNewFeatures() ? 0 : 1
-    } ]).then(function (answers) {
+    }]).then(function (answers) {
       if (answers.create_docs_issue) {
         github.issues.create({
           owner: owner,
           repo: repo,
           title: sake.getPluginName() + ': Updated to ' + sake.getPluginVersion(),
           body: sake.getPluginChanges() + (sake.options.release_issue_to_close ? '\r\n\r\nSee ' + sake.config.deploy.dev.owner + '/' + sake.config.deploy.dev.name + '#' + sake.options.release_issue_to_close : ''),
-          labels: [ sake.config.plugin.id.replace('woocommerce-', ''), 'docs', 'sales' ]
-        }, function (err, result) {
-          if (err) sake.throwError(err)
-
+          labels: [sake.config.plugin.id.replace('woocommerce-', ''), 'docs', 'sales']
+        }).then((result) => {
           log(result)
           log('Docs issue created!')
 
           done()
-        })
+        }).catch(sake.throwError)
       } else {
         log.warn('No docs issue was created for ' + sake.getPluginName())
         done()
@@ -188,15 +172,6 @@ module.exports = (gulp, plugins, sake) => {
       tasks.push(sake.options.deploy ? 'compress' : 'zip')
     }
 
-    let github = new GitHub({ debug: false })
-
-    github.authenticate({
-      type: 'basic',
-      // TODO: consider moving these to sake.config instead
-      username: process.env.GITHUB_USERNAME,
-      password: process.env.GITHUB_API_KEY
-    })
-
     log(`Creating GH release ${version} for ${owner}/${repo}`)
 
     tasks.push(function (cb) {
@@ -206,25 +181,25 @@ module.exports = (gulp, plugins, sake) => {
         tag_name: sake.options.prefix_release_tag ? sake.config.plugin.id + '-' + version : version,
         name: sake.getPluginName(false) + ' v' + version,
         body: sake.getPluginChanges()
-      }, function (err, result) {
-        if (err) sake.throwError('Creating GH release failed: ' + err.toString())
-
+      }).then((result) => {
         log('GH release created')
 
         sake.options.release_url = result.data.html_url
 
-        github.repos.uploadAsset({
+        github.repos.uploadReleaseAsset({
           url: result.data.upload_url,
           name: zipName,
           file: fs.readFileSync(zipPath),
           contentType: 'application/zip',
           contentLength: fs.statSync(zipPath).size
-        }, function (err) {
-          if (err) sake.throwError('Uploading release ZIP failed: ' + err.toString())
-
+        }).then(() => {
           log('Plugin zip uploaded')
           cb()
+        }).catch((err) => {
+          sake.throwError('Uploading release ZIP failed: ' + err.toString())
         })
+      }).catch((err) => {
+        sake.throwError('Creating GH release failed: ' + err.toString())
       })
     })
 
@@ -239,7 +214,7 @@ module.exports = (gulp, plugins, sake) => {
     createMilestones(tuesdays, done)
 
     // helper function to get all tuesdays of a year
-    function getTuesdays (y) {
+    function getTuesdays(y) {
       // ensure year is an integer
       y = parseInt(y, 10)
 
@@ -251,7 +226,7 @@ module.exports = (gulp, plugins, sake) => {
 
       while (y === d.getFullYear()) {
         let date = new Date(d.getTime())
-        tuesdays.push({date: date, name: `Deploy on ${dateFormat(date, 'mm/dd')}`})
+        tuesdays.push({ date: date, name: `Deploy on ${dateFormat(date, 'mm/dd')}` })
         d.setDate(d.getDate() + 7)
       }
 
@@ -266,7 +241,7 @@ module.exports = (gulp, plugins, sake) => {
     createMilestones(getMonthlyMilestones(year), done)
 
     // helper to get all months in a year
-    function getMonthlyMilestones (y) {
+    function getMonthlyMilestones(y) {
       // ensure year is an integer
       y = parseInt(y, 10)
 
@@ -274,7 +249,7 @@ module.exports = (gulp, plugins, sake) => {
 
       for (let i = 0; i < 12; i++) {
         let d = new Date(y, i + 1, 0)
-        months.push({date: d, name: dateFormat(d, 'mmmm yyyy')})
+        months.push({ date: d, name: dateFormat(d, 'mmmm yyyy') })
       }
 
       return months
@@ -285,16 +260,9 @@ module.exports = (gulp, plugins, sake) => {
   const createMilestones = (milestones, done) => {
     let owner = sake.options.owner || sake.config.deploy.dev.owner
     let repo = sake.options.repo || sake.config.deploy.dev.name
-    let github = new GitHub({ debug: false })
-
-    github.authenticate({
-      type: 'basic',
-      username: process.env.GITHUB_USERNAME,
-      password: process.env.GITHUB_API_KEY
-    })
 
     async.eachLimit(milestones, 5, function (milestone, cb) {
-      let description = codename.generate([ 'unique', 'alliterative', 'random' ], [ 'adjectives', 'animals' ]).join(' ')
+      let description = codename.generate(['unique', 'alliterative', 'random'], ['adjectives', 'animals']).join(' ')
       let formattedDate = dateFormat(milestone.date, 'yyyy-mm-dd')
 
       github.issues.createMilestone({
@@ -303,21 +271,19 @@ module.exports = (gulp, plugins, sake) => {
         title: milestone.name,
         description: description,
         due_on: milestone.date
-      }, function (err, result) {
-        if (err) {
-          // format the error for readability
-          let formattedError = err
-          try {
-            if (err.message) {
-              err = JSON.parse(err.message)
-            }
-            formattedError = JSON.stringify(err, null, 2)
-          } catch (e) {}
-          log.error(chalk.red(`Error creating milestone ${description} for ${formattedDate}:`) + '\n' + formattedError)
-        } else {
-          log.info(chalk.green(`Milestone ${description} for ${formattedDate} created!`))
-        }
+      }).then((result) => {
+        log.info(chalk.green(`Milestone ${description} for ${formattedDate} created!`))
         cb()
+      }).catch((err) => {
+        // format the error for readability
+        let formattedError = err
+        try {
+          if (err.message) {
+            err = JSON.parse(err.message)
+          }
+          formattedError = JSON.stringify(err, null, 2)
+        } catch (e) { }
+        log.error(chalk.red(`Error creating milestone ${description} for ${formattedDate}:`) + '\n' + formattedError)
       })
     }, function (error) {
       done(error)
