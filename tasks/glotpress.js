4061e45e0f444b4925e20e897221e5f262268e53
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2020-02-21 18:53:06 +0800

    Add translations:push command

--- tasks/glotpress.js
+++ tasks/glotpress.js
@@ -25,7 +25,7 @@ module.exports = (gulp, plugins, sake) => {
   // pull translations from GlotPress: downloads an updated .po file into /i18n/languages, then compiles it to matching .mo counterpart
   gulp.task('translations:pull', (done) => {
     async function getLanguages () {
-      const browser = await puppeteer.launch({ headless: false, dumpio: true })
+      const browser = await puppeteer.launch({ headless: false, dumpio: true, ignoreHTTPSErrors: true })
       const page = await browser.newPage()
 
       log.info('Fetching translations from ' + projectURL)
@@ -59,5 +59,64 @@ module.exports = (gulp, plugins, sake) => {
     exports.default = getLanguages()
 
     gulp.series('translations:compile')(done)
+  })
+
+  // push translation source to GlotPress: uploads an updated .pot file for the matching project corresponding to the current plugin
+  gulp.task('translations:push', (done) => {
+    async function pushPot () {
+      const username = process.env.GLOTPRESS_USER
+      const password = process.env.GLOTPRESS_PASSWORD
+      const browser = await puppeteer.launch({ headless: false, dumpio: true, ignoreHTTPSErrors: true, args: ['--disable-web-security'] })
+      const page = await browser.newPage()
+
+      log.info('Pushing translations source to ' + projectURL)
+
+      await page.goto(projectURL)
+
+      const title = await page.title()
+
+      log.info('Project page: ' + title)
+
+      const logIn = await page.$x("//a[contains(text(), 'Log in')]")
+
+      if (logIn.length > 0) {
+        await logIn[0].click()
+        await page.waitForNavigation()
+      } else {
+        throw new Error('Could not authenticate to GlotPress')
+      }
+
+      log.info('Logging in as admin...')
+
+      // log in as admin
+      await page.waitFor('input[name=log]')
+      await page.evaluate((text) => { (document.getElementById('user_login')).value = text }, username)
+      await page.waitFor('input[name=pwd]')
+      await page.evaluate((text) => { (document.getElementById('user_pass')).value = text }, password)
+      await page.waitFor('input[name=wp-submit]')
+      await page.click('#wp-submit')
+
+      log.info('Logged in!')
+
+      await page.goto(projectURL + 'import-originals/')
+
+      // get the ElementHandle of the selector above
+      const fileUpload = await page.$('input[name=import-file]')
+      const potFile = i18nPath + sake.config.translations.source
+
+      log.info('Uploading translations source file from ' + potFile)
+
+      await fileUpload.uploadFile(potFile)
+      await page.click('#submit')
+      await page.waitForNavigation()
+
+      log.info('Done!')
+
+      await browser.close()
+    }
+
+    exports.default = pushPot()
+
+    done()
   })
 }
