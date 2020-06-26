35856784560755ee2f91daed89d87ca22b21c120
Chase Wiseman <chase@skyverge.com>
2020-06-26 14:58:18 -0700

    Check composer status and clean the vendor folder before composer install

--- tasks/build.js
+++ tasks/build.js
@@ -5,10 +5,10 @@ module.exports = (gulp, plugins, sake) => {
   // - cleans the build directory
   // - compiles the plugin assets (linting where necessary)
   // - copies plugin files to the build directory
-  let tasks = ['clean:build', 'shell:composer_install', 'compile', 'copy:build']
+  let tasks = ['clean:build', 'shell:composer_status', 'clean:composer', 'shell:composer_install', 'compile', 'copy:build']
 
   if (sake.options['skip-composer']) {
-    tasks = _.without(tasks, 'shell:composer_install')
+    tasks = _.without(tasks, 'shell:composer_status', 'clean:composer', 'shell:composer_install')
   }
 
   gulp.task('build', gulp.series(tasks))
