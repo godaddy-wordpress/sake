d2a24b61e9bd11368b747f1abf8ff24bd2926d7b
Ashley Gibson <99189195+agibson-godaddy@users.noreply.github.com>
2025-01-13 01:57:17 +0000

    Update regex to prevent deleting entire file (#84)
    * Update regex so it doesn't match the entire CSS file!
    
    * Remove unused import

--- tasks/copy.js
+++ tasks/copy.js
@@ -151,7 +151,7 @@ module.exports = (gulp, plugins, sake) => {
 
     return gulp.src(paths, { base: sake.config.paths.src })
       .pipe(filter)
-      .pipe(plugins.replace(/^.*sourceMappingURL=.*$/mg, '')) // remove source mapping references - TODO: consider skipping sourcemaps in compilers instead when running build/deploy tasks
+      .pipe(plugins.replace(/\/\*# sourceMappingURL=.*?\*\/$/mg, '')) // remove source mapping references - TODO: consider skipping sourcemaps in compilers instead when running build/deploy tasks
       .pipe(plugins.replace('\n', '')) // remove an extra line added by libsass/node-sass
       .pipe(filter.restore)
       .pipe(gulp.dest(`${sake.config.paths.build}/${sake.config.plugin.id}`))
