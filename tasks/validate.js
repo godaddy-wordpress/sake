d05267bd6557157a5503bc4e012f1d0bd14da0b6
Ashley Gibson <agibson@godaddy.com>
2025-03-12 14:51:43 +0000

    Refactor validation task

--- tasks/validate.js
+++ tasks/validate.js
@@ -1,31 +1,36 @@
 import fs from 'node:fs'
 import log from 'fancy-log'
+import sake from '../lib/sake.js'
 
-module.exports = (gulp, plugins, sake) => {
-  gulp.task('validate:readme_headers', (done) => {
-    fs.readFile(`${sake.config.paths.src}/readme.txt`, 'utf8', (err, data) => {
-      if (err) sake.throwError(err)
+const validateReadmeHeaders = (done) => {
+  fs.readFile(`${sake.config.paths.src}/readme.txt`, 'utf8', (err, data) => {
+    if (err) sake.throwError(err)
 
-      let requiredHeaders = [
-        'License',
-        'License URI',
-      ]
+    let requiredHeaders = [
+      'License',
+      'License URI',
+    ]
 
-      if (sake.config.deploy.type === 'wp') {
-        requiredHeaders.push('Stable tag')
+    if (sake.config.deploy.type === 'wp') {
+      requiredHeaders.push('Stable tag')
+    }
+
+    requiredHeaders.forEach(headerName => {
+      log.info(`Validating readme.txt header ${headerName}`)
+      const regex = new RegExp(headerName + ':(.+)', 'ig')
+      const headerValue = data.match(regex)
+
+      if (! headerValue) {
+        sake.throwError('Missing required header in readme.txt: ' + headerName)
       }
-
-      requiredHeaders.forEach(headerName => {
-        log.info(`Validating readme.txt header ${headerName}`)
-        const regex = new RegExp(headerName + ':(.+)', 'ig')
-        const headerValue = data.match(regex)
-
-        if (! headerValue) {
-          sake.throwError('Missing required header in readme.txt: ' + headerName)
-        }
-      })
-
-      done()
     })
+
+    done()
   })
+}
+
+validateReadmeHeaders.displayName = 'validate:readme_headers'
+
+export {
+  validateReadmeHeaders
 }
