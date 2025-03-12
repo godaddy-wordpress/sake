f1be39f86633d39bcae46e39608b419a1a9206f8
Ashley Gibson <agibson@godaddy.com>
2025-03-12 14:50:20 +0000

    Move config to separate file that we can export

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 import gulp from 'gulp'
 import path from 'node:path'
 import fs from 'node:fs'
-import minimist from 'minimist'
 import log from 'fancy-log'
-import _ from 'lodash'
 import ForwardReference from 'undertaker-forward-reference'
 import dotenv from 'dotenv'
-import sakePlugin from './lib/sake'
 import gulpPlugins from 'gulp-load-plugins'
 import notifier from 'node-notifier'
 import stripAnsi from 'strip-ansi'
 import browserSync from 'browser-sync'
+import { fileURLToPath } from 'node:url'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // local .env file, overriding any global env variables
 let parentEnvPath = path.join('..', '.env')
@@ -41,112 +41,16 @@ if (fs.existsSync(devEnv)) {
 // enable forward-referencing tasks, see https://github.com/gulpjs/gulp/issues/1028
 gulp.registry(ForwardReference())
 
-// define default config
-let defaults = {
-  // sets up the plugin folder structure
-  paths: {
-    // Path to plugin source files - this is where the main plugin entry file is located. Set this to a dot (.) if the
-    // main plugin file and sake.config.js are in teh same directory. The path is relative to the current working directory.
-    // Mostly, this is the only path a plugin/repo needs to explicitly set
-    src: '.',
-    // where plugin assets are located, relative to `src`
-    assets: 'assets',
-    // where plugin CSS/SCSS assets are located, relative to `src`
-    css: 'assets/css',
-    // where plugin JS/COFFEE assets are located, relative to `src`
-    js: 'assets/js',
-    // where plugin image assets are located, relative to `src`
-    images: 'assets/img',
-    // where plugin font assets are located, relative to `src`
-    fonts: 'assets/fonts',
-    // the directory where plugin files are copied during the build task, relative to current working directory
-    build: 'build',
-    // path to the directory where production (WC and WP.org SVN) repos are cloned, may be an absolute path or relative to current working directory
-    tmp: '/tmp/sake',
-    // array of paths that should be excluded from the build
-    exclude: []
-  },
-
-  // Task-specific settings, set the key to task name and provide any settings as needed. Since sake uses Gulp behind the scenes
-  // and Gulp prefers code over configuration, there isn't a lot to do here. As you can see, some of these values can be defined
-  // as environment variables, as this makes more sense - ie whether you want to use browsersync or not is specific tp your local
-  // dev environment and workflow, not to a particular repo.
-  tasks: {
-    makepot: {
-      reportBugsTo: 'https://woocommerce.com/my-account/marketplace-ticket-form/',
-      domainPath: 'i18n/languages'
-    },
-    watch: {
-      useBrowserSync: process.env.USE_BROWSERSYNC || false
-    },
-    browserSync: {
-      url: process.env.BROWSERSYNC_URL || 'plugins-skyverge.test'
-    }
-  },
-
-  // which framework version this plugin uses - valid values: 'v5', 'v4', or pass boolean `false` to indicate a non-frameworked plugin
-  framework: 'v5',
-  // which deploy type does this plugin use - either 'wc' or 'wp', defaults to 'wc', specify `null` or `false` for no automated deploy
-  deploy: 'wc',
-  // the e-commerce platform this plugin is for, 'wc' or 'edd'
-  platform: 'wc'
-}
-
-// load local configuration
-// TODO: allow passing in config file path or config as string (for multi-plugin repos?)
-let localConfig = {}
-
-// support supplying a single / parent config file in multi-plugin repos
-let parentConfigPath = path.join(process.cwd(), '../sake.config.js')
-let found = false
-
-if (fs.existsSync(parentConfigPath)) {
-  log.warn('Found config file in parent folder')
-  localConfig = require(parentConfigPath)
-  found = true
-}
-
-// load local, plugin-specific config file
-let configFilePath = path.join(process.cwd(), 'sake.config.js')
-
-if (fs.existsSync(configFilePath)) {
-  localConfig = _.merge(localConfig, require(configFilePath))
-  found = true
-}
-
-if (!found) {
-  log.warn('Could not find local config file, using default config values.')
-}
-
-let config = _.merge(defaults, localConfig)
-
-// parse CLI options
-let options = minimist(process.argv.slice(2), {
-  boolean: ['minify'],
-  default: {
-    minify: true,
-    debug: false
-  }
+// @link https://github.com/jackfranklin/gulp-load-plugins/issues/141#issuecomment-2373391177
+let plugins = gulpPlugins({
+  config: path.resolve(__dirname, 'package.json')
 })
-
-const sake = sakePlugin(config, options)
-
-sake.initConfig()
-
-let plugins = gulpPlugins()
 
 // Attach browsersync as a plugin - not really a plugin, but it helps to
 // pass around the browsersync instance between tasks. Unfortunately, we
 // always have to load and create an instance of it, because gulp-if does not
 // support lazy evaluation yet: https://github.com/robrich/gulp-if/issues/75
 plugins.browserSync = browserSync.create()
-
-// load gulp plugins and tasks
-fs.readdirSync(path.join(__dirname, 'tasks')).forEach((file) => {
-  require(path.join(__dirname, 'tasks', file))(gulp, plugins, sake)
-})
-
-gulp.task('default', gulp.series('compile'))
 
 // show notification on task errors
 let loggedErrors = []
@@ -163,3 +67,8 @@ gulp.on('error', (event) => {
     loggedErrors.push(event.error)
   }
 })
+
+/************** Task Exports */
+
+export * from './tasks/bump.js'
+export * from './tasks/validate.js'
