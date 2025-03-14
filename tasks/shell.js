8f118fecd9891886a14af508fd9364dcf13afc5a
Ashley Gibson <agibson@godaddy.com>
2025-03-14 10:45:00 +0000

    Rework shell tasks

--- tasks/shell.js
+++ tasks/shell.js
@@ -3,261 +3,316 @@ import path from 'node:path'
 import log from 'fancy-log'
 import shell from 'shelljs'
 import _str from 'underscore.string'
+import sake from '../lib/sake.js'
+import gulp from 'gulp'
 
-module.exports = (gulp, plugins, sake) => {
-  const awk = process.platform === 'win32' ? 'gawk' : 'awk'
-  const noRunIfEmpty = process.platform !== 'darwin' ? '--no-run-if-empty ' : ''
+const awk = process.platform === 'win32' ? 'gawk' : 'awk'
+const noRunIfEmpty = process.platform !== 'darwin' ? '--no-run-if-empty ' : ''
 
-  // run a shell command, preserving output and failing the task on errors
-  const exec = (command, opts, done) => {
-    log.info('$ ' + command)
+// run a shell command, preserving output and failing the task on errors
+const exec = (command, opts, done) => {
+  log.info('$ ' + command)
 
-    if (typeof opts === 'function') {
-      done = opts
-      opts = {}
-    }
-
-    shell.exec(command, opts, (code) => done(code > 0 ? 'Command failed [exit code ' + code + ']: ' + command : null))
+  if (typeof opts === 'function') {
+    done = opts
+    opts = {}
   }
 
-  // update framework subtree
-  gulp.task('shell:update_framework', (done) => {
-    if (!sake.config.framework) {
-      sake.throwError('Not a frameworked plugin, aborting')
-    }
+  shell.exec(command, opts, (code) => done(code > 0 ? 'Command failed [exit code ' + code + ']: ' + command : null))
+}
 
-    let command = ''
+/**
+ * Update framework subtree
+ */
+const shellUpdateFramework = (done) => {
+  if (!sake.config.framework) {
+    sake.throwError('Not a frameworked plugin, aborting')
+  }
 
-    if (sake.config.framework === 'v4') {
-      let frameworkPath = path.join(process.cwd(), sake.config.paths.src, sake.config.paths.framework.base)
+  let command = ''
 
-      if (fs.existsSync(frameworkPath)) {
-        let branch = sake.options.branch || 'legacy-v4'
-        let prefix = path.join((sake.config.multiPluginRepo ? sake.config.plugin.id : '.'), sake.config.paths.framework.base)
-
-        command = [
-          'git fetch wc-plugin-framework ' + branch,
-          'git status',
-          'git subtree pull --prefix ' + prefix + ' wc-plugin-framework ' + branch + ' --squash',
-          'echo subtree up to date!'
-        ]
-
-        if (sake.config.multiPluginRepo) {
-          command.unshift('cd ../')
-        }
-
-        command = command.join(' && ')
-      } else {
-        command = 'echo no subtree to update'
-      }
-    }
-
-    exec(command, done)
-  })
-
-  // commit framework update
-  gulp.task('shell:update_framework_commit', (done) => {
+  if (sake.config.framework === 'v4') {
     let frameworkPath = path.join(process.cwd(), sake.config.paths.src, sake.config.paths.framework.base)
-    let command = ''
 
     if (fs.existsSync(frameworkPath)) {
+      let branch = sake.options.branch || 'legacy-v4'
+      let prefix = path.join((sake.config.multiPluginRepo ? sake.config.plugin.id : '.'), sake.config.paths.framework.base)
+
       command = [
-        'git add -A',
-        'git diff-index --quiet --cached HEAD || git commit -m "' + sake.config.plugin.name + ': Update framework to v' + sake.config.plugin.frameworkVersion + '"'
-      ].join(' && ')
-    } else {
-      // TODO: is this still necessary? {IT 2018-03-21}
-      command = [
-        'git add -A',
-        'git diff-index --quiet --cached HEAD || git commit -m "' + sake.config.plugin.name + ': Update readme.txt"'
+        'git fetch wc-plugin-framework ' + branch,
+        'git status',
+        'git subtree pull --prefix ' + prefix + ' wc-plugin-framework ' + branch + ' --squash',
+        'echo subtree up to date!'
       ]
-    }
 
-    exec(command, done)
-  })
-
-  // ensure the working copy (git tree) has no uncommited changes
-  gulp.task('shell:git_ensure_clean_working_copy', (done) => {
-    let command = [
-      'git diff-index --quiet HEAD'
-    ].join(' && ')
-
-    shell.exec(command, (code) => {
-      if (!code) return done()
-
-      shell.exec('git status', () => {
-        sake.throwError('Working copy is not clean!')
-      })
-    })
-  })
-
-  // commit and push update
-  gulp.task('shell:git_push_update', (done) => {
-    let command = [
-      'git add -A',
-      'git commit -m "' + sake.config.plugin.name + ': ' + sake.getPluginVersion() + ' Versioning"' + (sake.options.release_issue_to_close ? ' -m "Closes #' + sake.options.release_issue_to_close + '"' : ''),
-      'git push',
-      'echo git up to date!'
-    ].join(' && ')
-
-    exec(command, done)
-  })
-
-  // pull updates from WC repo, or clone repo, if deploying for the first time
-  gulp.task('shell:git_pull_wc_repo', (done) => {
-    let command = []
-
-    if (!fs.existsSync(sake.getProductionRepoPath())) {
-      // ensure that the tmp dir exists
-      if (!fs.existsSync(sake.config.paths.tmp)) {
-        shell.mkdir('-p', sake.config.paths.tmp)
+      if (sake.config.multiPluginRepo) {
+        command.unshift('cd ../')
       }
 
-      // repo does not exist yet
-      command = [
-        'cd ' + sake.config.paths.tmp,
-        'git clone ' + sake.config.deploy.production.url
-      ]
+      command = command.join(' && ')
     } else {
-      // repo already exists
-      command = [
-        'cd ' + sake.getProductionRepoPath(),
-        'git pull && git push'
-      ]
+      command = 'echo no subtree to update'
     }
+  }
 
-    shell.exec(command.join(' && '), (code, stdout, stderr) => {
-      // ignore missing ref error - this will happen when the remote repo is empty (for example, a new WC plugin)
-      // and we try to `git pull`
-      if (code === 1 && stderr.indexOf('Your configuration specifies to merge with the ref') > -1) {
-        return done()
-      } else {
-        return done(code > 0 ? 'Command failed [exit code ' + code + ']: ' + command : null)
-      }
-    })
-  })
+  exec(command, done)
+}
+shellUpdateFramework.displayName = 'shell:update_framework'
 
-  // commit and push update to WC repo
-  gulp.task('shell:git_push_wc_repo', (done) => {
-    let closeIssues = ''
+/**
+ * Commit framework update
+ */
+const shellUpdateFrameworkCommit = (done) => {
+  let frameworkPath = path.join(process.cwd(), sake.config.paths.src, sake.config.paths.framework.base)
+  let command = ''
 
-    if (sake.options.wc_issues_to_close) {
-      closeIssues = ' -m "' + _str.capitalize(sake.options.wc_issues_to_close.map((issue) => `closes #${issue}`).join(', ')) + '"'
-    }
-
-    // always ensure to pull the repo, but bypass the missing ref error
-    gulp.series('shell:git_pull_wc_repo')((err) => {
-      if (err) sake.throwError(err)
-
-      let command = [
-        'cd ' + sake.getProductionRepoPath(),
-        'git add -A',
-        'git commit -m "Update ' + sake.config.plugin.name + ' to ' + sake.getPluginVersion() + '"' + closeIssues,
-        'git push'
-      ].join(' && ')
-
-      exec(command, done)
-    })
-  })
-
-  // TODO: do we need this anymore?
-  // commit and push update, ver 2
-  gulp.task('shell:git_update_wc_repo', (done) => {
-    let command = [
-      'cd ' + sake.getProductionRepoPath(),
-      'git pull',
+  if (fs.existsSync(frameworkPath)) {
+    command = [
       'git add -A',
-      'git diff-index --quiet --cached HEAD || git commit -m "Updating ' + sake.config.plugin.name + '"',
-      'git push',
-      'echo WooCommerce repo up to date!'
+      'git diff-index --quiet --cached HEAD || git commit -m "' + sake.config.plugin.name + ': Update framework to v' + sake.config.plugin.frameworkVersion + '"'
     ].join(' && ')
+  } else {
+    // TODO: is this still necessary? {IT 2018-03-21}
+    command = [
+      'git add -A',
+      'git diff-index --quiet --cached HEAD || git commit -m "' + sake.config.plugin.name + ': Update readme.txt"'
+    ]
+  }
 
-    exec(command, done)
+  exec(command, done)
+}
+shellUpdateFrameworkCommit.displayName = 'shell:update_framework_commit'
+
+/**
+ * Ensure the working copy (git tree) has no uncommitted changes
+ */
+const shellGitEnsureCleanWorkingCopy = (done) => {
+  let command = [
+    'git diff-index --quiet HEAD'
+  ].join(' && ')
+
+  shell.exec(command, (code) => {
+    if (!code) return done()
+
+    shell.exec('git status', () => {
+      sake.throwError('Working copy is not clean!')
+    })
   })
+}
+shellGitEnsureCleanWorkingCopy.displayName = 'shell:git_ensure_clean_working_copy'
 
-  // stash uncommitted changes
-  gulp.task('shell:git_stash', (done) => {
-    exec('git stash', done)
-  })
+/**
+ * Commit and push update
+ */
+const shellGitPushUpdate = (done) => {
+  const command = [
+    'git add -A',
+    'git commit -m "' + sake.config.plugin.name + ': ' + sake.getPluginVersion() + ' Versioning"' + (sake.options.release_issue_to_close ? ' -m "Closes #' + sake.options.release_issue_to_close + '"' : ''),
+    'git push',
+    'echo git up to date!'
+  ].join(' && ')
 
-  // apply latest stash
-  gulp.task('shell:git_stash_apply', (done) => {
-    exec('git stash apply', done)
-  })
+  exec(command, done)
+}
+shellGitPushUpdate.displayName = 'shell:git_push_update'
 
-  gulp.task('shell:composer_status', (done) => {
-    if (fs.existsSync(path.join(process.cwd(), 'composer.json'))) {
-      exec('composer status -v', done)
-    } else {
-      log.info('No composer.json found, skipping composer status')
-      done()
-    }
-  })
+/**
+ * Pull updates from WC repo, or clone repo, if deploying for the first time
+ * @deprecated We no longer use WC mirror repos
+ */
+const shellGitPullWcRepo = (done) => {
+  let command = []
 
-  gulp.task('shell:composer_install', (done) => {
-    if (fs.existsSync(path.join(process.cwd(), 'composer.json'))) {
-      exec('composer install --no-dev', done)
-    } else {
-      log.info('No composer.json found, skipping composer install')
-      done()
-    }
-  })
-
-  gulp.task('shell:composer_update', (done) => {
-    if (fs.existsSync(path.join(process.cwd(), 'composer.json'))) {
-      exec('composer update --no-dev', done)
-    } else {
-      log.info('No composer.json found, skipping composer update')
-      done()
-    }
-  })
-
-  gulp.task('shell:svn_checkout', (done) => {
+  if (!fs.existsSync(sake.getProductionRepoPath())) {
     // ensure that the tmp dir exists
     if (!fs.existsSync(sake.config.paths.tmp)) {
       shell.mkdir('-p', sake.config.paths.tmp)
     }
 
-    let command = 'svn co --force-interactive ' + sake.config.deploy.production.url + ' ' + sake.getProductionRepoPath()
+    // repo does not exist yet
+    command = [
+      'cd ' + sake.config.paths.tmp,
+      'git clone ' + sake.config.deploy.production.url
+    ]
+  } else {
+    // repo already exists
+    command = [
+      'cd ' + sake.getProductionRepoPath(),
+      'git pull && git push'
+    ]
+  }
 
-    exec(command, done)
+  shell.exec(command.join(' && '), (code, stdout, stderr) => {
+    // ignore missing ref error - this will happen when the remote repo is empty (for example, a new WC plugin)
+    // and we try to `git pull`
+    if (code === 1 && stderr.indexOf('Your configuration specifies to merge with the ref') > -1) {
+      return done()
+    } else {
+      return done(code > 0 ? 'Command failed [exit code ' + code + ']: ' + command : null)
+    }
   })
+}
+shellGitPullWcRepo.displayName = 'shell:git_pull_wc_repo'
 
-  gulp.task('shell:svn_commit_trunk', (done) => {
-    const commitMsg = 'Committing ' + sake.getPluginVersion() + ' to trunk'
+/**
+ * Commit and push update to WC repo
+ * @deprecated We no longer use WC mirror repos
+ */
+const shellGitPushWcRepo = (done) => {
+  let closeIssues = ''
+
+  if (sake.options.wc_issues_to_close) {
+    closeIssues = ' -m "' + _str.capitalize(sake.options.wc_issues_to_close.map((issue) => `closes #${issue}`).join(', ')) + '"'
+  }
+
+  // always ensure to pull the repo, but bypass the missing ref error
+  gulp.series('shell:git_pull_wc_repo')((err) => {
+    if (err) sake.throwError(err)
 
     let command = [
-      'cd ' + path.join(sake.getProductionRepoPath(), 'trunk'),
-      'svn status | ' + awk + " '/^[?]/{print $2}' | xargs " + noRunIfEmpty + 'svn add',
-      'svn status | ' + awk + " '/^[!]/{print $2}' | xargs " + noRunIfEmpty + 'svn delete',
-      'svn commit --force-interactive --username="' + sake.config.deploy.production.user + '" -m "' + commitMsg + '"'
+      'cd ' + sake.getProductionRepoPath(),
+      'git add -A',
+      'git commit -m "Update ' + sake.config.plugin.name + ' to ' + sake.getPluginVersion() + '"' + closeIssues,
+      'git push'
     ].join(' && ')
 
     exec(command, done)
   })
+}
+shellGitPushWcRepo.displayName = 'shell:git_push_wc_repo'
 
-  gulp.task('shell:svn_commit_tag', (done) => {
-    const commitMsg = 'Tagging ' + sake.getPluginVersion()
+/**
+ * Commit and push update to WC repo, version 2
+ * @deprecated We no longer use WooCommerce mirror repos
+ */
+const shellGitUpdateWcRepo = (done) => {
+  let command = [
+    'cd ' + sake.getProductionRepoPath(),
+    'git pull',
+    'git add -A',
+    'git diff-index --quiet --cached HEAD || git commit -m "Updating ' + sake.config.plugin.name + '"',
+    'git push',
+    'echo WooCommerce repo up to date!'
+  ].join(' && ')
 
-    let command = [
-      'cd ' + path.join(sake.getProductionRepoPath(), 'tags', sake.getPluginVersion()),
-      'svn add .',
-      'svn commit --force-interactive --username="' + sake.config.deploy.production.user + '" -m "' + commitMsg + '"'
-    ].join(' && ')
+  exec(command, done)
+}
+shellGitUpdateWcRepo.displayName = 'shell:git_update_wc_repo'
 
-    exec(command, done)
-  })
+/**
+ * Stash uncommitted changes
+ */
+const shellGitStash = (done) => {
+  exec('git stash', done)
+}
+shellGitStash.displayName = 'shell:git_stash'
 
-  gulp.task('shell:svn_commit_assets', (done) => {
-    const commitMsg = 'Committing assets for ' + sake.getPluginVersion()
+/**
+ * Apply latest stash
+ */
+const shellGitStashApply = (done) => {
+  exec('git stash apply', done)
+}
+shellGitStashApply.displayName = 'shell:git_stash_apply'
 
-    let command = [
-      'cd ' + path.join(sake.getProductionRepoPath(), 'assets'),
-      'svn status | ' + awk + " '/^[?]/{print $2}' | xargs " + noRunIfEmpty + 'svn add',
-      'svn status | ' + awk + " '/^[!]/{print $2}' | xargs " + noRunIfEmpty + 'svn delete',
-      'svn commit --force-interactive --username="' + sake.config.deploy.production.user + '" -m "' + commitMsg + '"'
-    ].join(' && ')
+const shellComposerStatus = (done) => {
+  if (fs.existsSync(path.join(process.cwd(), 'composer.json'))) {
+    exec('composer status -v', done)
+  } else {
+    log.info('No composer.json found, skipping composer status')
+    done()
+  }
+}
+shellComposerStatus.displayName = 'shell:composer_status'
 
-    exec(command, done)
-  })
+const shellComposerInstall = (done) => {
+  if (fs.existsSync(path.join(process.cwd(), 'composer.json'))) {
+    exec('composer install --no-dev', done)
+  } else {
+    log.info('No composer.json found, skipping composer install')
+    done()
+  }
+}
+shellComposerInstall.displayName = 'shell:composer_install'
+
+const shellComposerUpdate = (done) => {
+  if (fs.existsSync(path.join(process.cwd(), 'composer.json'))) {
+    exec('composer update --no-dev', done)
+  } else {
+    log.info('No composer.json found, skipping composer update')
+    done()
+  }
+}
+shellComposerUpdate.displayName = 'shell:composer_update'
+
+const shellSvnCheckout = (done) => {
+  // ensure that the tmp dir exists
+  if (!fs.existsSync(sake.config.paths.tmp)) {
+    shell.mkdir('-p', sake.config.paths.tmp)
+  }
+
+  let command = 'svn co --force-interactive ' + sake.config.deploy.production.url + ' ' + sake.getProductionRepoPath()
+
+  exec(command, done)
+}
+shellSvnCheckout.displayName = 'shell:svn_checkout'
+
+const shellSvnCommitTrunk = (done) => {
+  const commitMsg = 'Committing ' + sake.getPluginVersion() + ' to trunk'
+
+  let command = [
+    'cd ' + path.join(sake.getProductionRepoPath(), 'trunk'),
+    'svn status | ' + awk + " '/^[?]/{print $2}' | xargs " + noRunIfEmpty + 'svn add',
+    'svn status | ' + awk + " '/^[!]/{print $2}' | xargs " + noRunIfEmpty + 'svn delete',
+    'svn commit --force-interactive --username="' + sake.config.deploy.production.user + '" -m "' + commitMsg + '"'
+  ].join(' && ')
+
+  exec(command, done)
+}
+shellSvnCommitTrunk.displayName = 'shell:svn_commit_trunk'
+
+const shellSvnCommitTag = (done) => {
+  const commitMsg = 'Tagging ' + sake.getPluginVersion()
+
+  let command = [
+    'cd ' + path.join(sake.getProductionRepoPath(), 'tags', sake.getPluginVersion()),
+    'svn add .',
+    'svn commit --force-interactive --username="' + sake.config.deploy.production.user + '" -m "' + commitMsg + '"'
+  ].join(' && ')
+
+  exec(command, done)
+}
+shellSvnCommitTag.displayName = 'shell:svn_commit_tag'
+
+const shellSvnCommitAssets = (done) => {
+  const commitMsg = 'Committing assets for ' + sake.getPluginVersion()
+
+  let command = [
+    'cd ' + path.join(sake.getProductionRepoPath(), 'assets'),
+    'svn status | ' + awk + " '/^[?]/{print $2}' | xargs " + noRunIfEmpty + 'svn add',
+    'svn status | ' + awk + " '/^[!]/{print $2}' | xargs " + noRunIfEmpty + 'svn delete',
+    'svn commit --force-interactive --username="' + sake.config.deploy.production.user + '" -m "' + commitMsg + '"'
+  ].join(' && ')
+
+  exec(command, done)
+}
+shellSvnCommitAssets.displayName = 'shell:svn_commit_assets'
+
+export {
+  shellUpdateFramework,
+  shellUpdateFrameworkCommit,
+  shellGitEnsureCleanWorkingCopy,
+  shellGitPushUpdate,
+  shellGitPullWcRepo,
+  shellGitPushWcRepo,
+  shellGitUpdateWcRepo,
+  shellGitStash,
+  shellGitStashApply,
+  shellComposerStatus,
+  shellComposerInstall,
+  shellComposerUpdate,
+  shellSvnCheckout,
+  shellSvnCommitTrunk,
+  shellSvnCommitTag,
+  shellSvnCommitAssets
 }
