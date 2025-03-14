47a42968bdfc4ff229b9935cb97b705a86c602f8
Ashley Gibson <agibson@godaddy.com>
2025-03-14 10:48:32 +0000

    Rework watch

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,43 +1,47 @@
 import log from 'fancy-log'
+import browserSync from 'browser-sync'
+import sake from '../lib/sake.js'
+import gulp from 'gulp'
 
-// watch files for changes and compile assets necessary
-module.exports = (gulp, plugins, sake) => {
-  gulp.task('watch', () => {
-    // start browsersync if enabled
-    if (sake.config.tasks.watch.useBrowserSync) {
-      let port = null
+const watch = (done) => {
+  // start browsersync if enabled
+  if (sake.config.tasks.watch.useBrowserSync) {
+    let port = null
 
-      const getPort = () => {
-        return port
-      }
-
-      plugins.browserSync.init({
-        proxy: {
-          target: sake.config.tasks.browserSync.url,
-          proxyReq: [(proxyReq) => {
-            proxyReq.setHeader('X-Forwarded-Host', 'localhost:' + getPort())
-          }]
-        }
-      }, (err, bs) => {
-        if (err) {
-          log.error(err)
-        }
-        port = bs.options.get('port')
-      })
+    const getPort = () => {
+      return port
     }
 
-    // allow other tasks to check if the watch task is running
-    sake.isWatching = true
+    browserSync.init({
+      proxy: {
+        target: sake.config.tasks.browserSync.url,
+        proxyReq: [(proxyReq) => {
+          proxyReq.setHeader('X-Forwarded-Host', 'localhost:' + getPort())
+        }]
+      }
+    }, (err, bs) => {
+      if (err) {
+        log.error(err)
+      }
+      port = bs.options.get('port')
+    })
+  }
 
-    // kick off the watchers
-    // TODO: consider breaking the pipes apart, so that we can only lint and compile the
-    // files that were actually changed (ie not all coffee files when only a single one was changed) {IT 2018-03-21}
-    gulp.watch(sake.config.paths.assetPaths.javascriptSources, gulp.parallel('scripts:js'))
-    gulp.watch(`${sake.config.paths.assetPaths.js}/**/*.coffee`, gulp.parallel('scripts:coffee'))
-    gulp.watch(`${sake.config.paths.assetPaths.css}/**/*.scss`, gulp.parallel('styles'))
-    // watching images will result in an endless loop, because imagemin changes the original files - a possible
-    // workaround would be to place all original images in a separate directory
-    // gulp.watch(`${sake.config.paths.assetPaths.images}/**.*{png,jpg,gif,svg}`, gulp.parallel('imagemin'))
-    // TODO: should we also watch for changes in PHP files and regenerate POT files and reload the browser?
-  })
+  // allow other tasks to check if the watch task is running
+  sake.isWatching = true
+
+  // kick off the watchers
+  // TODO: consider breaking the pipes apart, so that we can only lint and compile the
+  // files that were actually changed (ie not all coffee files when only a single one was changed) {IT 2018-03-21}
+  gulp.watch(sake.config.paths.assetPaths.javascriptSources, gulp.parallel('scripts:js'))
+  gulp.watch(`${sake.config.paths.assetPaths.js}/**/*.coffee`, gulp.parallel('scripts:coffee'))
+  gulp.watch(`${sake.config.paths.assetPaths.css}/**/*.scss`, gulp.parallel('styles'))
+  // watching images will result in an endless loop, because imagemin changes the original files - a possible
+  // workaround would be to place all original images in a separate directory
+  // gulp.watch(`${sake.config.paths.assetPaths.images}/**.*{png,jpg,gif,svg}`, gulp.parallel('imagemin'))
+  // TODO: should we also watch for changes in PHP files and regenerate POT files and reload the browser?
+}
+
+export {
+  watch
 }
