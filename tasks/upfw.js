c0a821b67f595eb50e743b60d0ff42309a1a63e4
Ashley Gibson <agibson@godaddy.com>
2025-03-17 12:46:11 +0000

    Update upfw tasks

--- tasks/upfw.js
+++ tasks/upfw.js
@@ -1,48 +1,61 @@
 import fs from 'node:fs'
 import path from 'node:path'
 import dottie from 'dottie'
+import sake from '../lib/sake'
+import gulp from 'gulp'
+import { bumpFrameworkVersionTask, bumpMinReqsTask } from './bump.js'
+import { shellComposerUpdateTask, shellUpdateFrameworkCommitTask, shellUpdateFrameworkTask } from './shell.js'
 
-module.exports = (gulp, plugins, sake) => {
-  // reload the framework version after updating the framework
-  gulp.task('set_framework_version', (done) => {
-    sake.config.plugin.frameworkVersion = sake.getFrameworkVersion()
-    done()
-  })
+/**
+ * Reload the framework version after updating the framework
+ */
+const setFrameworkVersionTask = (done) => {
+  sake.config.plugin.frameworkVersion = sake.getFrameworkVersion()
+  done()
+}
+setFrameworkVersionTask.displayName = 'set_framework_version'
 
-  // update framework to a specific version or branch
-  // example use: `sake upfw` or `sake upfw --backwards_compatible=4.4 --minimum_wc_version=2.5.5 --tested_up_to_wc_version=2.7.0 --minimum_wp_version=4.1 --tested_up_to_wp_version=4.6`
-  gulp.task('upfw', (done) => {
-    let tasks = [
-      'set_framework_version',
-      'bump:minreqs',
-      'shell:update_framework_commit'
-    ]
+/**
+ * Update the framework to a specific version or branch
+ * example use: `sake upfw` or `sake upfw --backwards_compatible=4.4 --minimum_wc_version=2.5.5 --tested_up_to_wc_version=2.7.0 --minimum_wp_version=4.1 --tested_up_to_wp_version=4.6`
+ */
+const updateFrameworkTask = (done) => {
+  let tasks = [
+    setFrameworkVersionTask,
+    bumpMinReqsTask,
+    shellUpdateFrameworkCommitTask
+  ]
 
-    if (sake.config.framework === 'v4' && !dottie.get(sake.config.composer, 'require.skyverge/wc-plugin-framework')) {
-      tasks.unshift('shell:update_framework')
+  if (sake.config.framework === 'v4' && !dottie.get(sake.config.composer, 'require.skyverge/wc-plugin-framework')) {
+    tasks.unshift(shellUpdateFrameworkTask)
+  }
+
+  if (sake.config.framework === 'v5') {
+    tasks.unshift(bumpFrameworkVersionTask)
+  }
+
+  // update composer
+  if (!sake.options['skip-composer-update']) {
+    // ensure FW version to update to is specified
+    if (!sake.options.framework_version) {
+      sake.throwError('Framework version not specified')
     }
 
-    if (sake.config.framework === 'v5') {
-      tasks.unshift('bump:framework_version')
-    }
+    dottie.set(sake.config.composer, 'require.skyverge/wc-plugin-framework', sake.options.framework_version)
 
-    // update composer
-    if (!sake.options['skip-composer-update']) {
-      // ensure FW version to update to is specified
-      if (!sake.options.framework_version) {
-        sake.throwError('Framework version not specified')
-      }
+    // update composer.json
+    fs.writeFileSync(path.join(process.cwd(), 'composer.json'), JSON.stringify(sake.config.composer, null, '  '))
 
-      dottie.set(sake.config.composer, 'require.skyverge/wc-plugin-framework', sake.options.framework_version)
+    tasks.unshift(shellComposerUpdateTask)
+  } else {
+    sake.options.framework_version = sake.config.plugin.frameworkVersion
+  }
 
-      // update composer.json
-      fs.writeFileSync(path.join(process.cwd(), 'composer.json'), JSON.stringify(sake.config.composer, null, '  '))
+  gulp.series(tasks)(done)
+}
+updateFrameworkTask.displayName = 'upfw'
 
-      tasks.unshift('shell:composer_update')
-    } else {
-      sake.options.framework_version = sake.config.plugin.frameworkVersion
-    }
-
-    gulp.series(tasks)(done)
-  })
+export {
+  setFrameworkVersionTask,
+  updateFrameworkTask
 }
