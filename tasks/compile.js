ac41a6df5005d22f0ecc8124cc7b5205e9128709
Ashley Gibson <agibson@godaddy.com>
2025-11-20 16:35:02 +0000

    Add flag to skip linting

--- tasks/compile.js
+++ tasks/compile.js
@@ -9,7 +9,11 @@ module.exports = (gulp, plugins, sake) => {
   // compile plugin assets
   gulp.task('compile', (done) => {
     // default compile tasks
-    let tasks = ['lint:php', 'scripts', 'styles', 'imagemin']
+    let tasks = ['scripts', 'styles', 'imagemin']
+
+    if (! process.argv.includes('--skip-linting')) {
+      tasks.push('lint:php')
+    }
 
     // unless exclusively told not to, generate the POT file as well
     if (!sake.options.skip_pot) {
